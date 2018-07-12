dd7f9a1bc86feab13d6a1746925e2c30f5b0023d
rcalazansn <rcalazansn@gmail.com>
2018-07-12 16:34:44 -0300

    inicio refatoracao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,52 +26,19 @@ export class HomePage {
     //modificar também os componentes do próprio zuck-modal que ficam salvos
     let zukeModal = (<HTMLDivElement>document.getElementById("zuck-modal"))
     if (zukeModal) {
-
       if (zukeModal.parentNode) {
         zukeModal.parentNode.removeChild(zukeModal);
       }
 
     }
 
-    var skins = {
-      'Snapgram': {
-        'avatars': true,
-        'list': false,
-        'autoFullScreen': false,
-        'cubeEffect': true
-      },
-
-      'VemDeZAP': {
-        'avatars': false,
-        'list': true,
-        'autoFullScreen': false,
-        'cubeEffect': false
-      },
-
-      'FaceSnap': {
-        'avatars': true,
-        'list': false,
-        'autoFullScreen': true,
-        'cubeEffect': false
-      },
-
-      'Snapssenger': {
-        'avatars': false,
-        'list': false,
-        'autoFullScreen': false,
-        'cubeEffect': false
-      }
-    };
-
-    let skin = "Snapgram"
-
     var stories = new (<any>window).Zuck('stories', {
       backNative: true,
-      autoFullScreen: skins[skin]['autoFullScreen'],
-      skin: skin,
-      avatars: skins[skin]['avatars'],
-      list: skins[skin]['list'],
-      cubeEffect: skins[skin]['cubeEffect'],
+      autoFullScreen: false,
+      skin: "Snapgram",
+      avatars: true,
+      list: false,
+      cubeEffect: true,
       localStorage: true,
       stories: this.storyService.getFeed()
     });
